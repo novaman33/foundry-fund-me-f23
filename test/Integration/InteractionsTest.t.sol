986bf7c328022c1e63964c47e4a8435b2d0cafaf
Martin Georgiev <martingeorgiev@Martins-MacBook-Air.local>
2023-11-05 13:03:46 +0200

    our first commit!

---
+++ test/Integration/InteractionsTest.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+
+contract InteractionsTest is Test {
+    FundMe fundMe;
+    address USER = makeAddr("user"); //creating an address for a made up user
+    uint256 constant SEND_VALUE = 0.1 ether; // value to be sent
+    uint256 constant STARTING_BALANCE = 10 ether;
+
+    function setUp() external {
+        DeployFundMe deploy = new DeployFundMe();
+        fundMe = deploy.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        assert(address(fundMe).balance == 0);
+    }
+}
